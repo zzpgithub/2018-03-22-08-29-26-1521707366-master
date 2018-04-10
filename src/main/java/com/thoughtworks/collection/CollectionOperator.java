468d2de8b6724b806d3c74c8d309e686b2813d0e
zzp <2269225970@qq.com>
2018-04-10 10:03:29 +0800

    Complete function getListByInterval()

--- src/main/java/com/thoughtworks/collection/CollectionOperator.java
+++ src/main/java/com/thoughtworks/collection/CollectionOperator.java
@@ -2,11 +2,22 @@ package com.thoughtworks.collection;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class CollectionOperator {
     public List<Integer> getListByInterval(int left, int right) {
-        throw new NotImplementedException();
+        //throw new NotImplementedException();
+        List<Integer> result = new ArrayList<>();
+        if (left <= right) {
+            for (int i = left; i <= right; i++)
+                result.add(i);
+        } else {
+            for (int i = left; i>=right; i--) {
+                result.add(i);
+            }
+        }
+        return result;
     }
 
     public List<Integer> getEvenListByIntervals(int left, int right) {
