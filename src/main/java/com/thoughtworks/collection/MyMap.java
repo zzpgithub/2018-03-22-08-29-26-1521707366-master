cd6760d99f6919cca14fb4792c64aabeec763df3
zzp <2269225970@qq.com>
2018-04-10 13:44:34 +0800

    Complete function getTriple

--- src/main/java/com/thoughtworks/collection/MyMap.java
+++ src/main/java/com/thoughtworks/collection/MyMap.java
@@ -2,6 +2,7 @@ package com.thoughtworks.collection;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -17,7 +18,11 @@ public class MyMap {
     }
 
     public List<Integer> getTriple() {
-        throw new NotImplementedException();
+        List<Integer> result = new ArrayList<>();
+        for (Integer integer : array) {
+            result.add(integer * 3);
+        }
+        return result;
     }
 
     public List<String> mapLetter() {
